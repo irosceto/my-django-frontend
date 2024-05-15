c6159e2e75adcd090aaa539d8528cf3b6896c85d
Aysenur1121 <nurero1121@gmail.com>
2024-05-15 23:11:06 +0300

    yonlendirme adresi degistirildi

--- src/signup.js
+++ src/signup.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import './signup.css';
-import { useNavigate } from 'react-router-dom'; // useNavigate kancasını içe aktar
 import { Link } from 'react-router-dom';
 
 const SignUp = () => {
@@ -11,27 +10,28 @@ const SignUp = () => {
     const [email, setEmail] = useState('');
     const [first_name, setFirstName] = useState('');
     const [last_name, setLastName] = useState('');
-    const navigate = useNavigate(); // useNavigate kancasını kullanarak yönlendirme yap
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
+    const handleSignUp = async (e) => {
+        e.preventDefault(); // Formun varsayılan davranışını engelle
         try {
             const response = await axios.post('http://localhost:8000/api/register/', {
-                username: username,
-                password: password,
-                email: email,
-                first_name: first_name,
-                last_name: last_name
+                username,
+                password,
+                email,
+                first_name,
+                last_name
             });
-
-            console.log(response.data.message); // veya başka bir mesaj
-            // Başarılı kayıt olduktan sonra Login sayfasına yönlendirme
-            navigate('/login'); // '/login' URL'sine yönlendirme
-
-        } catch (error) {
-            setErrorMessage(error.response.data.error);
-            console.error('Error:', error);
+            // Başarılı kayıt durumunda yönlendirme yap
+            if (response.status === 200) {
+                window.location.href = "/home"; // Yönlendirme
+            }
+        } 
+        catch (error) {
+            if (error.response.status === 400) {
+                setErrorMessage(error.response.data.message); // Sunucudan dönen özel hata mesajını göster
+            } else {
+                setErrorMessage("An error occurred. Please try again later."); // Diğer hata durumlarında genel bir mesaj göster
+            }
         }
     };
 
@@ -40,30 +40,28 @@ const SignUp = () => {
             <div className="card-body">
                 <div className="all_elements">
                     <div className="form-outer">
-                        <form id="form-input" onSubmit={handleSubmit}>
+                        <form id="form-input" onSubmit={handleSignUp}>
                             <i className="fa-solid fa-user"></i>
-                            <input type="text" name="username" placeholder="Enter your username" value={username} onChange={(e) => setUsername(e.target.value)} />
-                            <br/>
+                            <input type="text" name="username" placeholder="Enter your username" onChange={(e) => setUsername(e.target.value)} />
+                            <br />
                             <i className="fa-solid fa-user"></i>
-                            <input type="text" name="first_name" placeholder="Enter your Firstname" value={first_name} onChange={(e) => setFirstName(e.target.value)} />
-                            <br/>
+                            <input type="text" name="first_name" placeholder="Enter your Firstname" onChange={(e) => setFirstName(e.target.value)} />
+                            <br />
                             <i className="fa-solid fa-user"></i>
-                            <input type="text" name="last_name" placeholder="Enter your last name" value={last_name} onChange={(e) => setLastName(e.target.value)} />
-                            <br/>
+                            <input type="text" name="last_name" placeholder="Enter your last name" onChange={(e) => setLastName(e.target.value)} />
+                            <br />
                             <i className="fa-solid fa-lock"></i>
-                            <input type="password" name="password" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                            <br/>
-                            <i className="fa-solid fa-envelope"></i>
-                            <input type="email" name="email" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                            <br/>
-                            <Link to="/login" className="login">SIGNUP</Link>
-                            {/*<button type="submit" className="login" id="login-form-submit">SIGNUP</button>*/}{/*butonu çalıştıramadığım için link formatında yaptım*/}
+                            <input type="password" name="password" placeholder="Enter your password" onChange={(e) => setPassword(e.target.value)} />
+                            <br />
+                            <input type="email" name="email" placeholder="Enter your email" onChange={(e) => setEmail(e.target.value)} />
+                            <br />
+                            <input type="submit" className="login" value="SIGNUP" id="login-form-submit" />
                         </form>
                     </div>
                     <div className="login">
-                        <a className="login_button" href="login/">
+                        <Link className="login_button" to="/home">
                             You are already signup?
-                        </a>
+                        </Link>
                     </div>
                 </div>
             </div>
