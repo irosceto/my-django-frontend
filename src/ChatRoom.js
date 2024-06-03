fb817ed10e352ddf9b32e59ff388fec4b4f9465e
sha <seymaslan2511@outlook.com>
2024-06-03 23:31:28 +0300

    mesajlaşma yeri düzenlendi oda oluşturma düzenlendi

--- src/ChatRoom.js
+++ src/ChatRoom.js
@@ -91,12 +91,12 @@ const ChatRoom = ({ chatRoomId, accessToken, profilePicture }) => {
 
   return (
     <div className="chat-room-container">
-      <div className="top_div">
+      <div className="topbox_div">
         <div className="profile" onClick={handleProfileClick} style={{ cursor: 'pointer' }}>
           {profilePicture && <img src={`http://localhost:8000${profilePicture}`} alt="Profil Resmi" />}
         </div>
       </div>
-      <div className="container">
+      <div className="containerbox">
         <h2>Oda Kullanıcıları</h2>
         <ul>
           {members.map((member, index) => (
@@ -104,7 +104,7 @@ const ChatRoom = ({ chatRoomId, accessToken, profilePicture }) => {
           ))}
         </ul>
       </div>
-      <div className="third_div">
+      <div className="thirdbox_div">
         <div className="messages">
           {messages.map((message, index) => (
             <div key={index} className={`message ${message.sender === 'me' ? 'sent' : 'received'}`}>
@@ -121,7 +121,7 @@ const ChatRoom = ({ chatRoomId, accessToken, profilePicture }) => {
             onChange={(e) => setInputMessage(e.target.value)}
             placeholder="Mesajınızı yazın"
           />
-          <button onClick={sendMessage}>Gönder</button>
+          <button className='buttonsend' onClick={sendMessage}>Gönder</button>
         </div>
         {error && <div style={{ color: 'red' }}>{error}</div>}
       </div>
