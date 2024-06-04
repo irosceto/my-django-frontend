a4901ed62593c77292895bd20449bd98b875dd43
irosceto <irem2cetin@gmail.com>
2024-06-04 23:59:51 +0300

    Odadaki kullanıcılara link ekleme yapıldı

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {BrowserRouter, Routes, Route, Navigate, useParams} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, useParams } from 'react-router-dom';
 import SignUp from './signup';
 import LoginForm from './home';
 import Chat from './chat';
@@ -19,6 +19,7 @@ function App() {
           <Route path="/chat" element={accessToken ? <Chat accessToken={accessToken} /> : <Navigate to="/home" />} />
           <Route path="/chat/:roomId" element={accessToken ? <ChatRoomWrapper accessToken={accessToken} profilePicture={profilePicture} /> : <Navigate to="/home" />} />
           <Route path="/profile" element={accessToken ? <Profile accessToken={accessToken} onProfilePictureChange={setProfilePicture} /> : <Navigate to="/home" />} />
+
         </Routes>
       </BrowserRouter>
     </div>
@@ -27,7 +28,6 @@ function App() {
 
 const ChatRoomWrapper = ({ accessToken, profilePicture }) => {
   const { roomId } = useParams();
-
   return <ChatRoom chatRoomId={roomId} accessToken={accessToken} profilePicture={profilePicture} />;
 };
 
